a1095e6f05b073a746aa6d7f3397b68bb0b93968
Ayushek8 <139752769+Ayushek8@users.noreply.github.com>
2026-02-06 17:32:34 +0530

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -5,6 +5,9 @@ public class App {
     // ❌ Intentionally hardcoded secret for Gitleaks demo
     private static final String AWS_SECRET =
             "AKIA1234567890SECRETKEY";
+    // DEMO SECRET - DO NOT USE IN REAL CODE
+    private static final String AWS_ACCESS_KEY_ID =
+            "AKIAIOSFODNN7EXAMPLE";
 
     public static void main(String[] args) {
         System.out.println("Hello GitHub Actions Security Demo!");
